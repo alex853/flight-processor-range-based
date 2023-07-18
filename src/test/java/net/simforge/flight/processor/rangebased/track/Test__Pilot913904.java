7059eed6aa4a0a323ebfaccf4870db810725d78b
alex853 <alexxxey.kornev@gmail.com>
2023-07-18 13:11:05 +0300

    Added tests for my recent flights

--- src/test/java/net/simforge/flight/processor/rangebased/track/Test__Pilot913904.java
+++ src/test/java/net/simforge/flight/processor/rangebased/track/Test__Pilot913904.java
@@ -1,6 +1,7 @@
 package net.simforge.flight.processor.rangebased.track;
 
 import net.simforge.flight.processor.rangebased.Flight1;
+import net.simforge.flight.processor.rangebased.Track1;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -37,5 +38,59 @@ public class Test__Pilot913904 extends AbstractTrackTest {
         assertFlightRoute(flights.get(2), "UBBB", "LTCI");
         assertFlightRoute(flights.get(3), "LTCI", "LTCI");
         assertFlightRoute(flights.get(4), "LTCI", "LTAR");
+    }
+
+    @Test
+    public void test__2023_07_15__egll_lclk() throws IOException {
+        List<Flight1> flights = process(pilotNumber,
+                "/snapshots/pilot-913904_2023-07-15.csv");
+
+        assertEquals(1, flights.size());
+
+        Flight1 flight = flights.get(0);
+        assertFlight(flight,
+                "BAW6CA",
+                "A320",
+                "GEUYC",
+                "EGLL",
+                "LCLK",
+                true,
+                Track1.TrackingMode.Ideal);
+
+        assertFlightTimes(flight,
+                "08:16",
+                "08:41",
+                "12:55",
+                "12:59");
+
+        assertEquals(4.75, flight.getFlightTime(), 0.05);
+        assertEquals(4.25, flight.getAirTime(), 0.05);
+    }
+
+    @Test
+    public void test__2023_07_16__lclk_limc__non_completed_in_online() throws IOException {
+        List<Flight1> flights = process(pilotNumber,
+                "/snapshots/pilot-913904_2023-07-16.csv");
+
+        assertEquals(1, flights.size());
+
+        Flight1 flight = flights.get(0);
+        assertFlight(flight,
+                "BCS7061",
+                "B752",
+                "ECFTR",
+                "LCLK",
+                null,
+                false,
+                Track1.TrackingMode.Incomplete);
+
+        assertFlightTimes(flight,
+                "08:18",
+                "08:56",
+                null,
+                "12:08");
+
+        assertEquals(3.85, flight.getFlightTime(), 0.05);
+        assertEquals(3.25, flight.getAirTime(), 0.05);
     }
 }
